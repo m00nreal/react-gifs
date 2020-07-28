59ce45995f50f7c321432ecd54c10a746d78f418
Jair Luna <arel.jair@gmail.com>
2020-07-28 18:17:48 -0500

    prod dir

---
+++ docs/precache-manifest.b042743ec34021345254ad5bf1da1fdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5d675365ca0e59d2a6ffd0f7685ad1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0d76a4b76899afb292a",
+    "url": "/static/css/main.5d827764.chunk.css"
+  },
+  {
+    "revision": "a5b3e5b107070153ea50",
+    "url": "/static/js/2.e4a5b333.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4a5b333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0d76a4b76899afb292a",
+    "url": "/static/js/main.cee6ab0b.chunk.js"
+  },
+  {
+    "revision": "0d43baa4a829244cf185",
+    "url": "/static/js/runtime-main.73798afe.js"
+  }
+]);
